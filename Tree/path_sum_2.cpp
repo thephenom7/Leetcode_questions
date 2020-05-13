71d298200cb966d30f2d0835780ab877ef5d66ce
thephenom7 <akshaypuzhakkal07@gmail.com>
2020-05-13 18:54:14 +0530

    btbswe

---
+++ Tree/path_sum_2.cpp
@@ -0,0 +1,34 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<vector<int>> paths;
+    void calc(TreeNode *root,int sum,vector<int> vect)
+    {
+        if(!root)return;
+        vect.push_back(root->val);
+        if(sum==root->val && root->left==nullptr && root->right==nullptr)
+        {
+            paths.push_back(vect);
+            return;
+        }
+        vector<int> v (vect.begin(),vect.end());
+        calc(root->left,sum-root->val,v);
+        calc(root->right,sum-root->val,v);        
+    }
+    vector<vector<int>> pathSum(TreeNode* root, int sum) {
+        vector<int> vect;
+        calc(root,sum,vect);
+        return paths;
+        
+    }
+};
